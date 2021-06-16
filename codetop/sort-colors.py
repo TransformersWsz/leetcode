cc280efbf686f3644ca1add9bea9db72083e48e9
TransfoermersWsz <3287124026@qq.com>
2021-06-17 00:19:31 +0800

    ac 1 top

---
+++ codetop/sort-colors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/17 12:18 AM
+# @Author  : Swift
+# @File    : sort-colors.py
+# @Brief   : 75. 颜色分类
+# @Method  : https://leetcode-cn.com/problems/sort-colors/solution/yan-se-fen-lei-by-leetcode-solution/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p0 = p1 = 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] == 1:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 += 1
+            elif nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                if p0 < p1:
+                    nums[i], nums[p1] = nums[p1], nums[i]
+                p0 += 1
+                p1 += 1
--- test.py
+++ test.py
@@ -6,18 +6,10 @@ import collections
 import math
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        length = len(nums)
-        dp0 = [0]*length
-        dp1 = [0]*length
-        dp1[0] = nums[0]
-        for i in range(1, length):
-            dp0[i] = max(dp0[i-1], dp0[i-2]+nums[i])
-            if i != length - 1:
-                dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i])
-            else:
-                dp1[i] = dp1[i-1]
-        return max(dp0[length - 1], dp1[length-1])
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
 
 if __name__ == '__main__':
